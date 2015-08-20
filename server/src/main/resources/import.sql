237e3b29c2537eb5c49a203c98d5136938bd0f78
jntakpe <j.ntakpe@gmail.com>
2015-08-20 22:08:09 +0200

    Script d'import d'un utilisateur et des rôles

--- server/src/main/resources/import.sql
+++ server/src/main/resources/import.sql
@@ -1,1 +1,9 @@
 -- Utilisateurs
+INSERT INTO utilisateur (id, email, nom, prenom, telephone) VALUES (1, 'jntakpe@mail.com', 'NTAKPE', 'Jocelyn', '1234567890');
+
+-- Rôles
+INSERT INTO role (nom) VALUES ('ROLE_ADMIN');
+INSERT INTO role (nom) VALUES ('ROLE_USER');
+
+-- Rôles associés aux utilisateurs
+INSERT INTO utilisateur_role (utilisateur_id, role_nom) VALUES ((SELECT id FROM utilisateur WHERE email='jntakpe@mail.com'), (SELECT nom FROM role WHERE nom='ROLE_ADMIN'))
