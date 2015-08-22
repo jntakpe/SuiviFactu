d787efd55dcbb2c41a2b6b742f7abcfa1f8a3af7
jntakpe <j.ntakpe@gmail.com>
2015-08-22 16:47:56 +0200

    Menu refactoré sans icones

--- client/src/app/components/layout/sidebar-left/side-menu/side-menu-item.directive.js
+++ client/src/app/components/layout/sidebar-left/side-menu/side-menu-item.directive.js
@@ -1,84 +1,60 @@
 export default function sideMenuItemDirective($state) {
     'ngInject';
+
     return {
         restrict: 'E',
-        scope: {
+        scope: true,
+        bindToController: {
             item: '='
         },
-        template: '<div ng-include="itemTemplate"></div>',
-        link: function ($scope) {
-            // load a template for this directive based on the type ( link | dropdown )
-            $scope.itemTemplate = 'app/components/layout/sidebar-left/side-menu/side-menu-' + $scope.item.type + '.tmpl.html';
-            $scope.item.url = $state.href($scope.item.state);
-            /***
-             * Menu Click Handlers
-             ***/
-            $scope.toggleMenu = function () {
-                // send message down the menu from the parent, item is toggled
-                // this will close any sibling menus
-                $scope.$parent.$parent.$broadcast('toggleMenu', $scope.item, !$scope.item.open);
-            };
-
-            // this event ensures that any sibling menu items are closed when menu item is opened
-            $scope.$on('toggleMenu', function (event, item, open) {
-                // if this is the item we are looking for
-                if ($scope.item === item) {
-                    $scope.item.open = open;
-                }
-                else {
-                    $scope.item.open = false;
-                }
-            });
-
-            /***
-             * URL Change Handlers
-             ***/
-
-            function isActive() {
-                var params = $scope.item.params === undefined ? {} : $scope.item.params;
-                return $state.includes($scope.item.state, params);
-            }
-
-            // on first init check if we are the current menu item
-            if (isActive()) {
-                openMenu();
-            }
-
-            // opens the menu then calls its parents to also open
-            function openMenu() {
-                $scope.item.active = true;
-                $scope.item.open = true;
-                $scope.$emit('openParents');
-            }
-
-            // add a watch for when the url location changes
-            $scope.$on('$locationChangeSuccess', function () {
-                // location has changed so update the menu
-                $scope.item.active = false;
-                $scope.item.open = false;
-                if (isActive()) {
-                    openMenu();
-                }
-            });
-
-            // adds an extra hue class if the item is active
-            $scope.activeClass = function () {
-                return isActive() ? 'md-hue-3' : '';
-            };
-
-            $scope.openLink = function () {
-                // if we dont have any default params for this state just use empty object
-                var params = $scope.item.params === undefined ? {} : $scope.item.params;
-
-                $state.go($scope.item.state, params);
-            };
-
-            // this event is emitted up the tree to open parent menus
-            $scope.$on('openParents', function () {
-                // openParents event so open the parent item
-                $scope.item.active = true;
-                $scope.item.open = true;
-            });
-        }
+        template: '<div ng-include="sdm.itemTemplate"></div>',
+        controller: SideMenuController,
+        controllerAs: 'sdm'
     };
 };
+
+class SideMenuController {
+
+    constructor($scope, $state) {
+        'ngInject';
+        this.$scope = $scope;
+        this.$state = $state;
+        this.itemTemplate = `app/components/layout/sidebar-left/side-menu/side-menu-${this.item.type}.tmpl.html`;
+        this.item.url = $state.href(this.item.state);
+        this.activeClass = () => this.isActive() ? 'md-hue-3' : '';
+        this.openLink = () => $state.go(this.item.state, this.item.params || {});
+
+        if (this.isActive()) {
+            this.openMenu();
+        }
+
+        $scope.$on('toggleMenu', (event, item, open) => this.item.open = this.item === item ? open : false);
+
+        $scope.$on('$locationChangeSuccess', () => {
+            this.item.active = false;
+            this.item.open = false;
+            if (this.isActive()) {
+                this.openMenu();
+            }
+        });
+
+        $scope.$on('openParents', () => {
+            this.item.active = true;
+            this.item.open = true;
+        });
+    }
+
+    toggleMenu() {
+        this.$scope.$parent.$parent.$broadcast('toggleMenu', this.item, !this.item.open);
+    }
+
+    isActive() {
+        return this.$state.includes(this.item.state, this.item.params || {});
+    }
+
+    openMenu() {
+        this.item.active = true;
+        this.item.open = true;
+        this.$scope.$emit('openParents');
+    }
+}
